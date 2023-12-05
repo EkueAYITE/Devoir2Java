ebec31eda9b154a5b95bac50ec6a5f1d2219f6b4
Etienne <etiennejr.ayite@gmail.com>
2023-12-05 10:23:48 -0800

    graph1 et 2

--- src/main/java/sio/devoir2graphiques/Devoir2GraphiquesController.java
+++ src/main/java/sio/devoir2graphiques/Devoir2GraphiquesController.java
@@ -1,20 +1,33 @@
 package sio.devoir2graphiques;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import sio.devoir2graphiques.Tools.ConnexionBDD;
+import sio.devoir2graphiques.Tools.GraphiqueController;
+
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class Devoir2GraphiquesController implements Initializable
 {
+    HashMap<Integer,Double> datasGraphiqueMap1;
+    HashMap<String,Double> datasGraphiqueMap2;
+    ConnexionBDD maCnx;
+    XYChart.Series<Integer,Double> serieGraph1;
+    GraphiqueController graphiqueController;
     @FXML
     private Button btnGraph1;
     @FXML
@@ -43,7 +56,24 @@ public class Devoir2GraphiquesController implements Initializable
 
         // A vous de jouer
 
+        try {
+            maCnx = new ConnexionBDD();
+            graphiqueController = new GraphiqueController();
+            datasGraphiqueMap1 = graphiqueController.GetDataGraphique1();
+            Set<Integer> ages = datasGraphiqueMap1.keySet();
+            for (Integer age : ages) {
+                serieGraph1.getData().add(new XYChart.Data(age, datasGraphiqueMap1.get(age)));
+            }
+            graph1.getData().add(serieGraph1);
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+       
 
     @FXML
     public void menuClicked(Event event) throws SQLException {
@@ -53,6 +83,22 @@ public class Devoir2GraphiquesController implements Initializable
             apGraph1.toFront();
 
             // A vous de jouer
+            try {
+                maCnx = new ConnexionBDD();
+                graphiqueController = new GraphiqueController();
+                datasGraphiqueMap1 = graphiqueController.GetDataGraphique1();
+                Set<Integer> ages = datasGraphiqueMap1.keySet();
+                for (Integer age : ages) {
+                    serieGraph1.getData().add(new XYChart.Data(age, datasGraphiqueMap1.get(age)));
+                }
+                graph1.getData().add(serieGraph1);
+
+
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
         }
         else if(event.getSource() == btnGraph2)
@@ -61,6 +107,24 @@ public class Devoir2GraphiquesController implements Initializable
             apGraph2.toFront();
 
             // A vous de jouer
+            try {
+                maCnx = new ConnexionBDD();
+                graphiqueController = new GraphiqueController();
+                graph2.getData().clear();
+                ObservableList<PieChart.Data> observableListDataGraph2 = FXCollections.observableArrayList();
+                datasGraphiqueMap2 = graphiqueController.GetDataGraphique2();
+                Set<String> sexes = datasGraphiqueMap2.keySet();
+                for (String sexe : sexes) {
+                    observableListDataGraph2.add(new PieChart.Data(sexe, datasGraphiqueMap2.get(sexe)));
+                }
+                graph2.getData().add(serieGraph1);
+
+
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
         }
         else
--- src/main/java/sio/devoir2graphiques/Tools/GraphiqueController.java
+++ src/main/java/sio/devoir2graphiques/Tools/GraphiqueController.java
@@ -3,6 +3,8 @@ package sio.devoir2graphiques.Tools;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 
 public class GraphiqueController
 {
@@ -14,5 +16,52 @@ public class GraphiqueController
         cnx = ConnexionBDD.getCnx();
     }
     // A vous de jouer
+    public HashMap<Integer, Double> GetDataGraphique1() {
+        HashMap<Integer, Double> dataSalaires = new HashMap();
+            try
+            {
+                cnx = ConnexionBDD.getCnx();
+                ps = cnx.prepareStatement(" SELECT ageEmp, AVG(salaireEmp) AS moyenneSalaire\n" +
+                        "                    FROM employe\n" +
+                        "                    GROUP BY ageEmp;");
+                rs = ps.executeQuery();
+                while (rs.next()) {
+                    dataSalaires.put(rs.getInt(2),rs.getDouble(1));
+                }
+
+            } catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+
+
+        return dataSalaires;
+    }
+    public HashMap<String, Double> GetDataGraphique2() {
+        HashMap<String, Double> dataSatSex = new HashMap();
+        try
+        {
+            cnx = ConnexionBDD.getCnx();
+            ps = cnx.prepareStatement("SELECT\n" +
+                    "    sexe,\n" +
+                    "    COUNT(*) AS count\n" +
+                    "FROM\n" +
+                    "    employe\n" +
+                    "GROUP BY\n" +
+                    "    sexe;");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                dataSatSex.put(rs.getString(1),rs.getDouble(2));
+            }
+
+        } catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+
+        return dataSatSex;
+    }
+
 
 }
